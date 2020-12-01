36d76f0d615e492c9fcd90e0eb78c5ae2f71acfa
dare.baranova <dare.lebedeva@gmail.com>
2020-12-01 14:31:12 +0300

    Add css in component GameBody

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GameBody />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/GameBody.vue'
+import GameBody from './components/GameBody.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GameBody
   }
 }
 </script>
